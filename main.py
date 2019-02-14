c6275473fbc84a8ceea79ff58428fb75b7b0b5b5
angelbenedictoelena <angelbenedictoelena@gmail.com>
2019-02-14 18:36:49 +0100

    First commit (main.py)

---
+++ main.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  7 19:03:29 2019
+
+@author: angel
+"""
+
+import ODC
+import numpy as np
+   
+
+#inputFile = 'dc.in'
+inputFile = 'basic.txt'
+problem = ODC.readFile(inputFile)
+
+#ServerSize = 3
+#row = 1
+#col = 2
+##DataCenter,state, row,col = ODC.tryCollocate(DataCenter, ServerSize)
+##ODC.collocateServers(DataCenter,stackServer)
+#servers = ODC.collocateServers(DataCenter,stackServer)
+#pools = ODC.definePools(stackServer, Npools)
+#
+#print(pools)
+
+        
