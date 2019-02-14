48f8bca297846de068bd011c8285acc5d651752b
odri <mr.iakio@gmail.com>
2019-02-14 19:22:48 +0100

    primeras funcioncillas

--- ODC.py
+++ ODC.py
@@ -4,39 +4,42 @@ Created on Thu Feb  7 19:05:04 2019
 
 @author: angel
 """
-import numpy as np
 import random
 import math
+import numpy as np
+
 
 class problem:
     def __init__(self):
-        self.nRows   = 0
-        self.nSlots  = 0
-        self.nUnav   = 0
-        self.nPools  = 0
+        self.nRows = 0
+        self.nSlots = 0
+        self.nUnav = 0
+        self.nPools = 0
         self.nServers = 0
         self.uSlotList = []
         self.serverList = []
 
+
 class server:
     def __init__(self):
         self.size = []
-        self.capacity=[]
-        
+        self.capacity = []
+
+
 class allocatedServer:
-        def __init__(self):
-            self.server = []
-            self.row = []
-            self.slot = []
-            self.pool = []
-            
+    def __init__(self):
+        self.server = []
+        self.row = []
+        self.slot = []
+        self.pool = []
+
+
 class solution:
-        def __init__(self):
-            self.allocatedServerList = []
-            
-        
+    def __init__(self):
+        self.allocatedServerList = []
+
+
 def readFile(inputFile):
-    
     fid = open(inputFile, 'r')
     #    num = fid.read()
     lines = fid.readlines()
@@ -46,18 +49,18 @@ def readFile(inputFile):
         lineN += 1
         newLine = line.split(' ')
         if lineN == 1:
-            rows    = int(newLine[0])
-            slots   = int(newLine[1])
-            uslots  = int(newLine[2])
-            pools   = int(newLine[3])
+            rows = int(newLine[0])
+            slots = int(newLine[1])
+            uslots = int(newLine[2])
+            pools = int(newLine[3])
             servers = int(newLine[4])
-            stackUS = np.zeros((uslots,2))
-            stackServer = np.zeros((servers,2))
-        elif (lineN >= 2 and lineN<uslots+2):
-            stackUS[lineN-2][:] =  newLine
+            stackUS = np.zeros((uslots, 2))
+            stackServer = np.zeros((servers, 2))
+        elif (lineN >= 2 and lineN < uslots + 2):
+            stackUS[lineN - 2][:] = newLine
         else:
-            stackServer[lineN-2-uslots][:] =  newLine  
-        
+            stackServer[lineN - 2 - uslots][:] = newLine
+
     output.nPools = pools
     output.nrows = rows
     output.nServers = servers
@@ -68,72 +71,83 @@ def readFile(inputFile):
 
     return output
 
-def tryPosition(DataCenter,ServerSize, row, col):
-    MaxColumn   = np.size(DataCenter,1)
+
+def tryPosition(DataCenter, ServerSize, row, col):
+    MaxColumn = np.size(DataCenter, 1)
     state = 0
-    if ServerSize+col>MaxColumn:
+    if ServerSize + col > MaxColumn:
         state = 0
-    elif not all(item == 0 for item in DataCenter[row,col:col+ServerSize]):
+    elif not all(item == 0 for item in DataCenter[row, col:col + ServerSize]):
         state = 0
     else:
-        DataCenter[row,col:col+ServerSize]=1
+        DataCenter[row, col:col + ServerSize] = 1
         state = 1
-    
-    return DataCenter,state
-            
+
+    return DataCenter, state
+
+
 def tryCollocate(DataCenter, ServerSize):
-    MaxRow   = np.size(DataCenter,0)
-    MaxColumn   = np.size(DataCenter,1)
-    whileState   = 1
-    row0    = 0
-    col0    = 0
-    
+    MaxRow = np.size(DataCenter, 0)
+    MaxColumn = np.size(DataCenter, 1)
+    whileState = 1
+    row0 = 0
+    col0 = 0
+
     while whileState:
-        if (row0<MaxRow and col0 < MaxColumn):
-               DataCenter,ctry = tryPosition(DataCenter,ServerSize, row0, col0)
-               
+        if (row0 < MaxRow and col0 < MaxColumn):
+            DataCenter, ctry = tryPosition(DataCenter, ServerSize, row0, col0)
+
         if ctry:
             whileState = 0
             state = 1
             return DataCenter, state, row0, col0
-        elif col0<MaxColumn:
+        elif col0 < MaxColumn:
             col0 += 1
-        elif row0<MaxRow:
+        elif row0 < MaxRow:
             col0 = 0
             row0 += 1
         else:
             state = 0
             return DataCenter, state, row0, col0
-        
-def collocateServers(DataCenter,stackServer):
+
+
+def collocateServers(DataCenter, stackServer):
     output = []
     for server in stackServer:
-        DataCenter, success, row0, col0= tryCollocate(DataCenter, int(server[0]))
+        DataCenter, success, row0, col0 = tryCollocate(DataCenter, int(server[0]))
         if success:
-            output.append([row0,col0])
+            output.append([row0, col0])
         else:
-           output.append('x')
+            output.append('x')
     return output
+
 
 def definePools(stackServer, Npools):
     pools = []
     nServers = len(stackServer)
-    vecServers = np.linspace(0,nServers-1,nServers)
+    vecServers = np.linspace(0, nServers - 1, nServers)
     random.shuffle(vecServers)
-    nSerPool = math.floor(nServers/Npools)
+    nSerPool = math.floor(nServers / Npools)
     for i in range(Npools):
-        if i == Npools-1:
-            pools.append(vecServers[i*nSerPool:])
+        if i == Npools - 1:
+            pools.append(vecServers[i * nSerPool:])
         else:
-            pools.append(vecServers[i*nSerPool:(i+1)*nSerPool])
-    
+            pools.append(vecServers[i * nSerPool:(i + 1) * nSerPool])
+
     return pools
 
-def gCapacity(stackServer,pools,servers):
+
+def gCapacity(stackServer, pools, servers):
     ok = 1
-    
-    
-    
-    
-    
-              
+
+    return False
+
+
+def get_poolRows(solution, pool):
+    rows = [elem.row for elem in solution if elem.pool == pool]
+    return set(rows)
+
+
+def get_poolguaranteedcapacity(problem, pool, solution):
+    capacities = [get_rowcapacity(problem, row, pool, solution) for row in range(0, problem.nRows)]
+    return sum(capacities) - max(capacities)
