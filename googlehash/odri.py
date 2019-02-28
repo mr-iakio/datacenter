04be3894ef8f493e1a881d728d6c0e4994380d4d
Odri <mr.iakio@gmail.com>
2019-02-28 18:17:00 +0100

    preparando hash

---
+++ googlehash/odri.py
@@ -0,0 +1,9 @@
+class problem:
+    def __init__(self):
+        self.nRows = 0
+        self.nSlots = 0
+        self.nUnav = 0
+        self.nPools = 0
+        self.nServers = 0
+        self.uSlotList = []
+        self.serverList = []
