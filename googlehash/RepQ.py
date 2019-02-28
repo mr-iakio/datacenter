5ee6fcf059476aea5415ef3d2a7a6ef750f6604a
angelbenedictoelena <angelbenedictoelena@gmail.com>
2019-02-28 19:30:35 +0100

    readfile function

--- googlehash/RepQ.py
+++ googlehash/RepQ.py
@@ -1,47 +1,17 @@
-class problem:
-    def __init__(self):
-        self.nRows = 0
-        self.nSlots = 0
-        self.nUnav = 0
-        self.nPools = 0
-        self.nServers = 0
-        self.uSlotList = []
-        self.serverList = []
+class Photo:
+   def __init__(self, orient, tags):
+       self.orient = orient
+       self.tags = tags
 
-
-def readFile(inputFile):
+def read_file(inputFile):
     fid = open(inputFile, 'r')
     #    num = fid.read()
     lines = fid.readlines()
-    lineN = 0
-    output = problem()
-    for line in lines:
-        lineN += 1
+    # print('The number of lines is: ',len(lines))
+    output = []
+    for line in lines[1:]:
         newLine = line.strip()
         newLine = newLine.split(' ')
-        if lineN == 1:
-            rows = int(newLine[0])
-            slots = int(newLine[1])
-            uslots = int(newLine[2])
-            pools = int(newLine[3])
-            servers = int(newLine[4])
-            stackUS = []
-            stackServer = []
-        elif (lineN >= 2 and lineN < uslots + 2):
-            stackUS.append([int(newLine[0]), int(newLine[1])])
-        else:
-            stackServer.append([int(newLine[0]), int(newLine[1])])
-
-    output.nPools = pools
-    output.nRows = rows
-    output.nServers = servers
-    output.nSlots = slots
-    output.nUnav = uslots
-    for i in range(0, output.nServers):
-        output.serverList.append(server(stackServer[i][0], stackServer[i][1]))
-    output.uSlotList = stackUS
-
+        cPhoto = Photo(newLine[0], newLine[2:])
+        output.append(cPhoto)
     return output
-
-def dummy():
-    print('The repository is loaded')
--- googlehash/main.py
+++ googlehash/main.py
@@ -4,16 +4,19 @@ Created on Thu Feb  7 19:03:29 2019
 
 @author: angel
 """
-import RepQ
+from RepQ import *
+import time
 import random
 import numpy as np
 
 ############## INPUT FILE ##############
-print('The code is running!')
-RepQ.dummy()
-inputFile = 'dc.in'
-# # inputFile = 'basic.txt'
-# problem = ODC.readFile(inputFile)
-# dataCenterInit = problem.uSlotList[:]
+
+inputFile = 'a_example.txt' #4 photos
+# inputFile = 'b_lovely_landscapes.txt' #80000 photos
+# inputFile = 'c_memorable_moments.txt' #1000 photos
+# inputFile = 'd_pet_pictures.txt' #90000 photos
+# inputFile = 'e_shiny_selfies.txt' #80000 photos
+
+photos = read_file(inputFile)
 
 ################ GENETIC ALGORITHM #################
